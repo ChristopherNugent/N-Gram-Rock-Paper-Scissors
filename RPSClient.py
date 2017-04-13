a72c4d3b9cccf4ba955408e2079c9ad141a101c0
Chris <awedeven@gmail.com>
2017-04-13 15:05:06 -0700

    Now saves past patterns. Improved interface.

--- RPSClient.py
+++ RPSClient.py
@@ -1,5 +1,5 @@
 from Player import Player
-
+from os import mkdir
 
 def check_win(first, second):
     """Returns 0 if draw, 1 if first wins, 2 if second wins"""
@@ -7,7 +7,7 @@ def check_win(first, second):
     f, s = space.index(first), space.index(second)
     return (f - s + 3) % 3
 
-    
+
 def get_move():
     while True:
         user_move = input('Please enter your move (r, p, s) or q to quit: ')
@@ -17,10 +17,19 @@ def get_move():
 
 
 def main(mem=100):
+    name = input('What is your name? --> ')
     player = Player(mem)
-    move = ''
-    score = [0, 0]
+    try:
+        with open('Samples/' + name + '.txt', 'r') as f:
+            for line in f:
+                for ch in line:
+                    player.add_move(ch)
+    except FileNotFoundError:
+        mkdir('Samples')
+        with open('Samples/' + name + '.txt', 'w') as f:
+            pass
 
+    score = [0, 0]
     user_move = get_move()
     while user_move != 'q':
         if user_move == 'debug':
@@ -28,7 +37,8 @@ def main(mem=100):
         else:
             player_move = player.get_move()
             player.add_move(user_move)
-            print('You played ' + user_move + ' and the AI played ' + player_move)
+            print('You played ' + user_move +
+                  ' and the AI played ' + player_move)
             win = check_win(user_move, player_move)
             if win == 0:
                 print('Draw!')
@@ -40,8 +50,11 @@ def main(mem=100):
                 score[1] += 1
             print('The score is ' + str(score[0]) + ' - ' + str(score[1]))
             print('\n')
-            
+
+            with open('Samples/' + name + '.txt', 'a') as f:
+                f.write(user_move)
         user_move = get_move()
+
 
 if __name__ == '__main__':
     main()
